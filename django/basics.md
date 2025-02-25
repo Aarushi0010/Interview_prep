4e2c6c7654769a960bf93e07c4e31f0a3594709b
Aarushi0010 <mahajanaarushi10@gmail.com>
2025-02-25 23:51:07 +0530

    static,templates loaded

--- django/basics.md
+++ django/basics.md
@@ -1,6 +1,6 @@
 ## what we intially did : <br>
 1. Install venv - pip install virtualenv <br>
-2. Rename venv - virtualenv "env" <br>
+2. Rename venv - virtualenv "venv" <br>
 3. open env - env_name\Scripts\Activate<br>
 4. Install Django - pip install Django <br>
 <br>
--- django/learning/learning/views.py
+++ django/learning/learning/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("welcome to Home Page")
+    # return HttpResponse("welcome to Home Page")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("This is About page")
