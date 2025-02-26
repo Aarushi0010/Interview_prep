f050ef55f72af1ed0dfeba5c618f2339abc430f7
Aarushi0010 <mahajanaarushi10@gmail.com>
2025-02-26 19:28:54 +0530

    bubble sort

---
+++ DSA/sorting/bubble_sort.py
@@ -0,0 +1,23 @@
+# sorting where we compare the two items and swap to push the maximum to last position
+
+def bubbblesort(arr):
+    n = len(arr)
+    for i in range(n ):
+        swapped = False 
+
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j] , arr[j+1] = arr[j+1] , arr[j]
+                swapped = True
+
+        if(swapped == False):
+            break 
+
+if __name__ =='__main__':
+    arr = [13, 46 , 24 , 52 , 20 , 9]
+
+    print("given array : " , arr)
+
+    bubbblesort(arr)
+    print("sorted array : " , arr)
+
