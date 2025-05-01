4923f2b8b5803f196cbead4a25729ad285880dab
Aarushi0010 <mahajanaarushi10@gmail.com>
2025-05-01 18:46:17 +0530

    LL medium

---
+++ DSA/linked_list/deleteMiddle.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteMiddle(self, head: ListNode) -> ListNode:
+        slow , fast = head , head
+        prev = None
+
+        if head is None or head.next is None:
+            return None 
+
+        while fast and fast.next:
+            prev = slow 
+            slow = slow.next
+            fast = fast.next.next
+
+        prev.next = slow.next 
+        return head
+    
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
+solution = Solution()
+
+result = solution.deleteMiddle(head)
+
+while result:
+    print(result.val)
+    result = result.next
